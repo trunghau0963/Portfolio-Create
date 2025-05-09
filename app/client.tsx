1de87a2b1970dfbd6bb3421a04e8b08056fe8114
Nguyễn Trần Trung Hậu <96047179+trunghau0963@users.noreply.github.com>
2025-05-09 10:53:24 +0700

    modify loading

--- app/client.tsx
+++ app/client.tsx
@@ -27,7 +27,7 @@ function ClientLayout({ children }: { children: ReactNode }) {
   if (isLoading) {
     return (
       <div className="min-h-screen flex items-center justify-center">
-        <Loader2 className="h-8 w-8 animate-spin text-gray-500" />
+        <Loader2 className="h-8 w-8 animate-spin text-gray-500 dark:text-gray-100" />
       </div>
     )
   }
