485069123beed0e44e6cc1056ae7ca85010ae2be
nguyen tran trung hau <ntthau0963@gmail.com>
2025-10-10 17:46:34 +0700

    fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FC = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-sm text-white/60">
-              &copy; {new Date().getFullYear()} Portfolio. Nguyen Tran Trung Hau.
+              &copy; {new Date().getFullYear()} Portfolio. Trung-Hau Nguyen.
             </p>
           </div>
           
